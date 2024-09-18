753f9d50719d94ad88163d896fb22b5e1eba8ce2
pein <plants.coordinators@gmail.com>
2024-09-18 20:48:55 +0800

    Remove unnecessary folder cache

--- app/dependency/header.py
+++ app/dependency/header.py
@@ -3,11 +3,6 @@ from typing import Annotated
 import jwt
 from fastapi import Header, HTTPException
 
-
-async def auth_token_header(x_token: Annotated[str, Header()]):
-    if x_token != "fake-super-secret-token":
-        raise HTTPException(status_code=400, detail="X-Token header invalid")
-
 async def get_token_header(x_token: Annotated[str, Header()]):
     try:
         jwt.decode(x_token, "secret", algorithms=["HS256"])
